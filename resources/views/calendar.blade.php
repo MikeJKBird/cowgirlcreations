83c2458e5b37ae2c857a1d0abae2faba561f1a8c
MikeJKBird <mike.bird@outlook.com>
2016-03-15 14:09:02 -0700

    format carbon dates in views

--- resources/views/calendar.blade.php
+++ resources/views/calendar.blade.php
@@ -13,7 +13,7 @@
 
         <h3>Events</h3>
         @foreach ($events as $event)
-            <a href="calendar/{{$event->id}}">{{ $event->name }}</a> : {{$event->date}}
+            <a href="calendar/{{$event->id}}">{{ $event->name }}</a>: {{$event->date->toFormattedDateString()}}
             <hr>
         @endforeach
     </div>
--- resources/views/event.blade.php
+++ resources/views/event.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <h1>{{ $event->name }}</h1>
         <p>Located at: {{$event->location}}</p>
-        <p>Occuring on:  {{$event->date}}</p>
+        <p>Occuring:  {{$event->date->diffForHumans()}}</p>
         <p>There is room for {{$event->maxNumberParticipants}} racers</p>
         <button>Sign up now!</button>
     </div>
