d114595ddfa06b615f4da9b06b5c2ba9c97ce308
CodingABetterWorld <mike.bird@outlook.com>
2016-05-10 22:31:04 -0700

    styling + EOD

--- resources/views/photos.blade.php
+++ resources/views/photos.blade.php
@@ -12,7 +12,7 @@
     <div class="container">
         <div class="row">
             @foreach($photos as $photo)
-                <a href="/img/{{$photo->name}}" data-lightbox="gallery" data-title="{{$photo->caption}}"><img src="/img/{{$photo->name}}" class="col-xs-6 col-sm-4 col-md-3 photo-gallery-item" /></a>
+                <a href="/img/{{$photo->name}}" data-lightbox="gallery" data-title="{{$photo->caption}}" class="col-xs-6 col-sm-4 col-md-3"><img src="/img/{{$photo->name}}" class="photo-gallery-item" /></a>
             @endforeach
         </div>
     </div>
