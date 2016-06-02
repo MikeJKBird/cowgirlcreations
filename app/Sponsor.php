87992b9e5f9f583a4ce5962658eae94fa26b9281
CodingABetterWorld <mike.bird@outlook.com>
2016-06-01 20:57:29 -0700

    fix typo

--- app/Sponsor.php
+++ app/Sponsor.php
@@ -4,7 +4,7 @@ namespace App;
 
 use Illuminate\Database\Eloquent\Model;
 
-class cSponsor extends Model
+class Sponsor extends Model
 {
     protected $fillable = [
         'name', 'website', 'value', 'logo'
