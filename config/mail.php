4404bf6e446c840070630fe5321a359a3fd8f525
CodingABetterWorld <mike.bird@outlook.com>
2016-07-12 22:53:40 -0700

    fix sender email info

--- config/mail.php
+++ config/mail.php
@@ -55,7 +55,7 @@ return [
     |
     */
 
-    'from' => ['address' => null, 'name' => null],
+    'from' => ['address' => 'cowgirlcreations@yahoo.com', 'name' => 'Sherri-Lynn Prest'],
 
     /*
     |--------------------------------------------------------------------------
